f714ab77f271a5db8de5a6f251192e4542208f77
Uddhav Ganpati Kamble <uddhavkamble356@gmail.com>
2026-09-12 19:36:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     // origin:"http://localhost:5173",
-    origin:"ec2-13-60-63-20.eu-north-1.compute.amazonaws.com",
+    origin:"http://ec2-13-60-63-20.eu-north-1.compute.amazonaws.com",
     credentials:true
 }))
 
